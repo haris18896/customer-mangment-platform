2e151ed06bdbe43c587829673748e23c52f0b453
HarisAhmad43 <haris18896@gmail.com>
2020-07-29 05:33:45 -0700

    database modeled

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    phone = models.CharField(max_length=200, null=True, blank=True)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY=(
+        ('Indoor','Indoor'),
+        ('out door','out door'),
+    )
+    name = models.CharField(max_length=200, null=True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
+    category = models.CharField(max_length=200, null=True, blank=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('out for delivery','out for delivery'),
+        ('Delivered','Delivered')
+    )
+    # cusotmer =
+   # product =
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)     #e.g Delivered,pending or out for delivery e.t.c
+
