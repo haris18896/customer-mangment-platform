2e266fa16e11fca6b00aebf60714980874a32bcf
HarisAhmad43 <haris18896@gmail.com>
2020-07-29 14:44:38 -0700

    the problem was in urls.py

--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from crm1.accounts import  views
+from . import  views
 urlpatterns = [
     path('', views.home, name="home"),
     path('products/', views.products, name="products"),
