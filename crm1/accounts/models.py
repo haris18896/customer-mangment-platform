5a09f56b7783a269a9192bc680e59181a0b0c87f
HarisAhmad43 <haris18896@gmail.com>
2020-08-08 11:33:13 -0700

    user authentication added

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -1,30 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+
 class Customer(models.Model):
-    name = models.CharField(max_length=200, null=True, blank=True)
-    phone = models.CharField(max_length=200, null=True, blank=True)
-    email = models.CharField(max_length=200, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
 
+
 class Tag(models.Model):
-    name = models.CharField(max_length=200, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    CATEGORY=(
-        ('Indoor','Indoor'),
-        ('out door','out door'),
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
     )
-    name = models.CharField(max_length=200, null=True, blank=True)
-    price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
-    category = models.CharField(max_length=200, null=True, blank=True, choices=CATEGORY)
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     tags = models.ManyToManyField(Tag)
@@ -36,14 +40,15 @@ class Product(models.Model):
 class Order(models.Model):
     STATUS = (
         ('Pending', 'Pending'),
-        ('out for delivery','out for delivery'),
-        ('Delivered','Delivered')
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
     )
+
     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-    status = models.CharField(max_length=200, null=True, choices=STATUS)     #e.g Delivered,pending or out for delivery e.t.c
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    note = models.CharField(max_length=1000, null=True)
 
     def __str__(self):
         return self.product.name
-
